d242efb4ec86efea22de764400412ef7f65bd166
Ahd <dafuq.ahd@gmail.com>
2020-07-19 20:16:52 +0300

    render FullPage components and inside it Slides that hold each section; add the background component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
+import { FullPage, Slide } from "react-full-page";
+import styled from "styled-components";
+import Background from "./components/Background";
 
+const Layout = styled.div``;
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Layout>
+      <Background />
+      <FullPage>
+        <Slide>
+          <Header />
+        </Slide>
+        <Slide>
+          <Header />
+        </Slide>
+      </FullPage>
+    </Layout>
+  );
 }
 
 export default App;
