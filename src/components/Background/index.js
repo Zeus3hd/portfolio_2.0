4b0331b776d0fa7568a86b55fe6a11c9023fd9cd
Ahd <dafuq.ahd@gmail.com>
2020-07-19 20:43:52 +0300

    render the background squares

--- src/components/Background/index.js
+++ src/components/Background/index.js
@@ -1,10 +1,44 @@
 import React from "react";
-import { Wrapper, TopRightRect, BottomLeftRect } from "./index.style";
+import {
+  Wrapper,
+  TopRightRect,
+  BottomLeftRect,
+  TopLeftRectsContainer,
+  BottomRightRectsContainer,
+  TopLeftRectsContainerInner,
+  BottomRightRectsContainerInner,
+  Rect1,
+  Rect2,
+  Rect3,
+  Rect4,
+  Rect5,
+  Rect6,
+} from "./index.style";
 const Background = () => {
   return (
     <Wrapper>
       <TopRightRect />
       <BottomLeftRect />
+      <TopLeftRectsContainer>
+        <TopLeftRectsContainerInner>
+          <Rect1 />
+          <Rect2 />
+          <Rect3 />
+          <Rect4 />
+          <Rect5 />
+          <Rect6 />
+        </TopLeftRectsContainerInner>
+      </TopLeftRectsContainer>
+      <BottomRightRectsContainer>
+        <BottomRightRectsContainerInner>
+          <Rect1 />
+          <Rect2 />
+          <Rect3 />
+          <Rect4 />
+          <Rect5 />
+          <Rect6 />
+        </BottomRightRectsContainerInner>
+      </BottomRightRectsContainer>
     </Wrapper>
   );
 };
