c314abd0d01d37feabd2dd19f8655b06c7d19924
Ahd <dafuq.ahd@gmail.com>
2020-07-19 22:17:30 +0300

    render Experience component

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import { FullPage, Slide } from "react-full-page";
 import styled from "styled-components";
 import Background from "./components/Background";
 import Services from "./components/Services";
+import Experience from "./components/Experience";
 const Layout = styled.div``;
 function App() {
   return (
     <Layout>
       <Background />
       <FullPage>
+        <Slide>
+          <Experience />
+        </Slide>
         <Slide>
           <Header />
         </Slide>
