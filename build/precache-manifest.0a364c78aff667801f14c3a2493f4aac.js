a4dcf10aa7a2b3a76c18e8a9c64eae8f5aeb66c3
Ahd <dafuq.ahd@gmail.com>
2020-08-04 21:32:29 +0300

    fixes

---
+++ build/precache-manifest.0a364c78aff667801f14c3a2493f4aac.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5dafce4a372a5916b0a8e77e5bc2d3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5957f921ab5b882a5b89",
+    "url": "/static/css/main.8db5fd9b.chunk.css"
+  },
+  {
+    "revision": "31151de401ba144e7843",
+    "url": "/static/js/2.d7f6c973.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d7f6c973.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5957f921ab5b882a5b89",
+    "url": "/static/js/main.60b340ee.chunk.js"
+  },
+  {
+    "revision": "2f49460bd712681f6dbe",
+    "url": "/static/js/runtime-main.2014d4d2.js"
+  },
+  {
+    "revision": "57561406b0ac5baa1b9cd8d923a9ff82",
+    "url": "/static/media/services_ecommerce.57561406.png"
+  },
+  {
+    "revision": "a39760586515cc39226e9f58a687bc8e",
+    "url": "/static/media/services_mobile.a3976058.png"
+  },
+  {
+    "revision": "ecad8adb09a137f417b53f16c16a1bd1",
+    "url": "/static/media/services_web.ecad8adb.png"
+  }
+]);
